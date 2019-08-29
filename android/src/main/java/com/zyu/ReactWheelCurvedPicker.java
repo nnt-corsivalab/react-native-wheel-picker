ad91d965746b491c142c1423172ba998eefe2034
nnt-corsivalab <53160374+nnt-corsivalab@users.noreply.github.com>
2019-08-29 15:07:20 +0700

    Fix selected line color

--- android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
@@ -57,7 +57,7 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
         int colorFrom = 0x00FFFFFF;//Color.BLACK;
-        int colorTo = Color.TRANSPARENT;
+        int colorTo = Color.BLACK;
         LinearGradient linearGradientShader = new LinearGradient(rectCurItem.left, rectCurItem.top, rectCurItem.right/2, rectCurItem.top, colorFrom, colorTo, Shader.TileMode.MIRROR);
         paint.setShader(linearGradientShader);
         canvas.drawLine(rectCurItem.left, rectCurItem.top, rectCurItem.right, rectCurItem.top, paint);
